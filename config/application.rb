7187a719da90f6c3c3d6eb4eb23fda0f9fde3672
enoqueJonas <enoque.macanda@gmail.com>
2023-06-07 00:03:18 +0200

    UPDATe cors policy

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module HelloRorApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://127.0.0.1:4000' # Replace with the actual origin of your React app
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
   end
 end
